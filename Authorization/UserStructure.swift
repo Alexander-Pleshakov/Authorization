5a3c500324a27b1622e441b5fca3c82adb57d63d
Pleshakov-Alexandr <sanya.pleshakov.95@mail.ru>
2023-11-15 00:29:14 +0300

    Created the first page user interface

---
+++ Authorization/UserStructure.swift
@@ -0,0 +1,9 @@
+//
+//  UserStructure.swift
+//  Authorization
+//
+//  Created by Александр Плешаков on 15.11.2023.
+//
+
+import Foundation
+
--- Authorization/ViewController.swift
+++ Authorization/ViewController.swift
@@ -8,7 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var registrationButton: UIButton!
+    @IBOutlet weak var newPasswordButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
